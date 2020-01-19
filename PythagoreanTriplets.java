c4aaf8552a2bbaa0ba9acf4e0a5773ad8dcc5a5d
BhardwajSiddharth <59619690+BhardwajSiddharth@users.noreply.github.com>
2020-01-19 20:20:56 +0530

    Add files via upload

---
+++ PythagoreanTriplets.java
@@ -0,0 +1,24 @@
+package Algorithm;
+import java.util.Scanner;
+public class PythagoreanTriplets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+	   int a,b,c;
+	   System.out.println("Enter Three Numbers...");
+	   a=sc.nextInt();
+	   b=sc.nextInt();
+	   c=sc.nextInt();
+	   if(a*a+b*b==c*c)
+	   {
+		   System.out.println("The numbers Entered are pythagorean triplets ...");
+	   }
+	   else {
+		   System.out.println("The numbers Entered are not pythagorean triplets ...");
+	   }
+	   
+	}
+}
+	    
+
